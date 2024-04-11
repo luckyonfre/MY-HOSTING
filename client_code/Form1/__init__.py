e94781dddb6d43f80b30a50044538a31eecc70f7
reshamverma593@gmail.com <reshamverma593@gmail.com>
2024-04-11 11:46:01 +0000

    Created acehost.png and www.acehost.com.png; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,3 +15,8 @@ class Form1(Form1Template):
 
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
